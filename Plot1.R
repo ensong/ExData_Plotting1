cfb019a8e8256f14d2d2c58ccab7242640acf419
ensong <song.edward@gmail.com>
2015-11-08 13:58:47 -0700

    updated plot 1 code

--- Plot1.R
+++ Plot1.R
@@ -4,23 +4,7 @@
 setwd("C:/Users/Ed/Documents/R Exploratory Data Analysis Course")
 
 #Imports the data set
-Data <- read.table("household_power_consumption.txt"
-                   ,sep=";"
-                   ,header=T
-                   ,colClasses = c('character', 'character', 'numeric',
-                                   'numeric', 'numeric', 'numeric',
-                                   'numeric', 'numeric', 'numeric')
-                   ,na.string = '?'
-                   ,fill=FALSE
-                   ,strip.white=TRUE)
 
-Data$DateTime <- strptime(paste(Data$Date, Data$Time), "%d/%m/%Y %H:%M:%S")
-Data <- subset(Data, as.Date(DateTime) >= as.Date("2007-02-01") & 
-                 as.Date(DateTime) <= as.Date("2007-02-02"))
-
-
-# Read the data and take subset of two days in 02/07 as mentioned in 
-# assignment
 data <- read.table('household_power_consumption.txt', sep=';', header=T, 
                    colClasses = c('character', 'character', 'numeric',
                                   'numeric', 'numeric', 'numeric',
